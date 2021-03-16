8319a9fb21ea1cd29e09a9fc7bc10ed16fbb6f57
unknown <you@example.com>
2021-03-16 20:54:48 +0900

    helloworld2

--- helloworldproject/views.py
+++ helloworldproject/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def helloworldfunction(request):
   returnobject = HttpResponse("<h1>Hello World</h1>")
   return returnobject
+
+class HelloWorldClass(TemplateView):
+  template_name = 'hello.html'
